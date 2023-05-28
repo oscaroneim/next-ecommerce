6b18e7f847a32b46cc47b0ea6ec1beabcf9b96e3
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2023-05-28 20:44:18 +0100

    added webhooks for stripe and prisma/Started the dashboard page api call

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,26 @@
-export default function Dashboard(){
+import { PrismaClient } from '@prisma/client'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { getServerSession } from 'next-auth'
+
+
+
+const fetchOrders = async () => {
+    const prisma = new PrismaClient()
+    const user = await getServerSession(authOptions)
+    if(!user){
+        return { message: "Not logged in " }
+    }
+    const orders = await prisma.order.findMany({
+        where: { userId: user?.user?.id },
+        include: {products: true}
+    })
+    return orders
+}
+
+
+export default async function Dashboard(){
+    const orders = await fetchOrders()
+    console.log(orders)
     return(
 <div>
     <h1>Dashboard</h1>
--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -23,36 +23,36 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
         const sig = req.headers["stripe-signature"]
 
         if(!sig){
-            return res.status(400).send("Missing stripe signature")
+            return res.status(400).send("Missing stripe signature")                       //-------------------- error if signature is missing
         }
 
-        let event: Stripe.Event     //------------ Handles different types of events
+        let event: Stripe.Event                       //------------ Handles different types of events
 
         try {
-            event = stripe.webhooks.constructEvent(
+            event = stripe.webhooks.constructEvent(              //------------- the event needs buffer with the request, a signature from stripe and the secret to function
                 buf,
                 sig,
                 process.env.STRIPE_WEBHOOK_SECRET!
             )
-        }  catch (err) {
+        }  catch (err) {                                            //------------ if something is missing from the constructEvent throw an error
             return res.status(400).send("Webhook error" + err)
         }
-        switch (event.type) {
-            case "payment_intent.created":
+        switch (event.type) {                              //-------------------- case no1, event if payment intent has been created
+            case "payment_intent.created": 
                 const paymentIntent = event.data.object
                 console.log("Payment intent was created")
                 break
-            case "charge.succeeded":
+            case "charge.succeeded":                                 //-------------------- case no2 event if the paymentIntnentID matches the charge succeed
                 const charge= event.data.object as Stripe.Charge
                 if(typeof charge.payment_intent === "string"){
-                  const order = await prisma.order.update({
+                  const order = await prisma.order.update({                          //-------------------- update the order field on the DB by Id and setting the statue to complete
                     where: { paymentIntentID: charge.payment_intent},
                     data: {status: "complete"},
                   })
                 }
                 break
                 default:
-                    console.log("Unhandled event type:" + event.type)
+                    console.log("Unhandled event type:" + event.type)                   
              }
-             res.json({ received: true})
+             res.json({ received: true })                  //-------------------- this to confirm the event has been received 
   }

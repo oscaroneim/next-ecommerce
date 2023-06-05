de8db254d8ac84405ee0069ee4e50a09f6f1f081
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2023-06-05 20:38:06 +0100

    product page div removed

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,7 +5,7 @@ import AddCart from "./AddCart";
 
 export default async function Product({ searchParams }: SearchParamTypes) {               //-------------- here we are fetching the product details based on the id (refer to SearchParamTypes)
     return (
-  <div className="flex">     
+      
 <div className="flex flex-col 2xl:flex-row items-center justify-between gap-16">
    <Image 
       src={searchParams.image} 
@@ -19,17 +19,19 @@ export default async function Product({ searchParams }: SearchParamTypes) {     
          <p>{searchParams.description}</p>
       <p className="py-2">{searchParams.features}</p>
         <div className="flex gap-2">
-             <p className="font-bold text-teal-700">{searchParams.unit_amount !== null ? formatPrice(searchParams.unit_amount) : "N/A"}</p>
+             <p className="font-bold text-teal-700">
+                {searchParams.unit_amount !== null ? formatPrice(searchParams.unit_amount) : "N/A"}
+                </p>
         </div>
-            <AddCart 
+        <AddCart 
             id={searchParams.id}
             name={searchParams.name}
             unit_amount={searchParams.unit_amount}
             image={searchParams.image}
               />
-            </div>
-        </div>
-</div> 
+     </div>
+ </div>
+
     )
 }
 
